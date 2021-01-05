35c7dd084354bbc2b11f434a69f3088a5f31a20f
Alexey Zaytsev <alex.rv9rh@gmail.com>
2021-01-05 18:32:26 +0500

    Add light off timer, constants..

--- auto_night_light/auto_night_light.ino
+++ auto_night_light/auto_night_light.ino
@@ -8,6 +8,14 @@ int led1 = 0;
 int roomSensorDistance;
 int roomDistance = 100;
 
+bool lightIsOn = false;
+
+int MAIN_CYCLE_DELAY = 500;
+int TIMER_MULTIPLIER = 2;
+
+int LIGHT_OFF_TIMER_VALUE = 5000;
+int lightOffTimer = 0;
+
 
 void setup() {
   //Serial.begin(9600);
@@ -24,10 +32,21 @@ void loop() {
   roomSensorDistance = ultrasonic.distanceRead();
 
   if (roomSensorDistance < roomDistance) {
+
     digitalWrite(led1, HIGH);
+    lightIsOn = true;
+    lightOffTimer = LIGHT_OFF_TIMER_VALUE;
+
   } else {
-    digitalWrite(led1, LOW);
+
+  	if (lightOffTimer > 0) {
+  		lightOffTimer = lightOffTimer - (MAIN_CYCLE_DELAY * TIMER_MULTIPLIER);
+  	} else {
+    	digitalWrite(led1, LOW);
+    	lightIsOn = false;
+  	}
+
   }
   
-  delay(250);
+  delay(MAIN_CYCLE_DELAY);
 }
