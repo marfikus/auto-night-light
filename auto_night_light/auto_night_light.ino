7551c2106f4e11432381994cbbab5e66edfd703a
Alexey Zaytsev <alex.rv9rh@gmail.com>
2021-01-08 22:09:13 +0500

    It works!

--- auto_night_light/auto_night_light.ino
+++ auto_night_light/auto_night_light.ino
@@ -6,10 +6,11 @@ const int MAIN_CYCLE_DELAY = 100;
 const int LIGHT_OFF_TIMER_VALUE = 3000;
 const int FADEOUT_STEP = 5;
 const int MAX_ALARM_COUNT = 3;
+const int SENSORS_COUNT = 3;
 
 const int KITCHEN_THRESHOLD_DISTANCE = 20;
-const int ROOM_THRESHOLD_DISTANCE = 150;
-const int HALL_THRESHOLD_DISTANCE = 250;
+const int ROOM_THRESHOLD_DISTANCE = 15;
+const int HALL_THRESHOLD_DISTANCE = 25;
 
 enum sensorStates {
 	NORMAL,
@@ -26,7 +27,7 @@ struct usSensor {
 	int alarmCounter;
 };
 
-usSensor sensors[1];
+usSensor sensors[SENSORS_COUNT];
 
 Ultrasonic us1(2, 3);
 Ultrasonic us2(4, 5);
@@ -60,7 +61,7 @@ void lightOn() {
 
 void lightOff() {
 
-	for (int i = 0; i < sizeof(sensors); i++) {
+	for (int i = 0; i < SENSORS_COUNT; i++) {
 		if (sensors[i].state == ALARM) {
 			return;
 		}
@@ -88,7 +89,7 @@ void lightOff() {
 
 
 void setup() {
-  Serial.begin(9600);
+  // Serial.begin(9600);
 
 	pinMode(led1, OUTPUT);
 	pinMode(led2, OUTPUT);
@@ -100,46 +101,52 @@ void setup() {
 
 	// кухня
 	// sensors[0].ultrasonic(2, 3);
+	// можно так инициализировать сенсоры
 	// sensors[0] = (usSensor) {
 	// 	NORMAL, 0, 0, KITCHEN_THRESHOLD_DISTANCE, 0
 	// };
+	// но так, кмк, более понятно сразу, нагляднее
 	sensors[0].state = NORMAL;
 	sensors[0].timer = 0;
 	sensors[0].currentDistance = 0;
 	sensors[0].thresholdDistance = KITCHEN_THRESHOLD_DISTANCE;
 	sensors[0].alarmCounter = 0;
 
-	// // комната
-	// // sensors[1].ultrasonic(4, 5);
-	// sensors[1].state = NORMAL;
-	// sensors[1].timer = 0;
-	// sensors[1].currentDistance = 0;
-	// sensors[1].thresholdDistance = ROOM_THRESHOLD_DISTANCE;
-	// sensors[1].alarmCounter = 0;
+	// комната
+	// sensors[1].ultrasonic(4, 5);
+	sensors[1].state = NORMAL;
+	sensors[1].timer = 0;
+	sensors[1].currentDistance = 0;
+	sensors[1].thresholdDistance = ROOM_THRESHOLD_DISTANCE;
+	sensors[1].alarmCounter = 0;
 
-	// // коридор
-	// // sensors[2].ultrasonic(6, 7);
-	// sensors[2].state = NORMAL;
-	// sensors[2].timer = 0;
-	// sensors[2].currentDistance = 0;
-	// sensors[2].thresholdDistance = HALL_THRESHOLD_DISTANCE;
-	// sensors[2].alarmCounter = 0;
+	// коридор
+	// sensors[2].ultrasonic(6, 7);
+	sensors[2].state = NORMAL;
+	sensors[2].timer = 0;
+	sensors[2].currentDistance = 0;
+	sensors[2].thresholdDistance = HALL_THRESHOLD_DISTANCE;
+	sensors[2].alarmCounter = 0;
 }
 
 
 void loop() {
 
 	sensors[0].currentDistance = us1.distanceRead();
-	// sensors[1].currentDistance = us2.distanceRead();
-	// sensors[2].currentDistance = us3.distanceRead();
+	sensors[1].currentDistance = us2.distanceRead();
+	sensors[2].currentDistance = us3.distanceRead();
 
-	// Serial.print("============");
-	// Serial.println(i);
-	Serial.println(sensors[0].currentDistance);		
-	// Serial.println("============");
+	// Serial.println(sensors[0].currentDistance);
+	// Serial.println(sensors[1].currentDistance);
+	// Serial.println(sensors[2].currentDistance);
 
-	for (int i = 0; i < sizeof(sensors); i++) {
+	for (int i = 0; i < SENSORS_COUNT; i++) {
 		// sensors[i].currentDistance = sensors[i].ultrasonic.distanceRead();
+
+		// Serial.print("Sensor ");
+		// Serial.print(i);
+		// Serial.print(" = ");
+		// Serial.println(sensors[i].currentDistance);
 
 		if (sensors[i].currentDistance < sensors[i].thresholdDistance) {
 
@@ -151,20 +158,6 @@ void loop() {
 				sensors[i].state = ALARM;
 				sensors[i].timer = LIGHT_OFF_TIMER_VALUE;
 				sensors[i].alarmCounter = 0;
-
-				// switch (sensors[i].state) {
-				// 	case NORMAL:
-				// 		lightOn();
-				// 		sensors[i].state = ALARM;
-				// 		sensors[i].timer = LIGHT_OFF_TIMER_VALUE;
-				// 		break;
-
-				// 	case ALARM:
-				// 		lightOn();
-				// 		sensors[i].timer = LIGHT_OFF_TIMER_VALUE;
-				// 		break;
-				// }
-			
 			}
 		} else {
 
@@ -179,44 +172,5 @@ void loop() {
 	}
 
 	lightOff();
-
-
-
-  // // Serial.print("Distance in CM: ");
-  // roomSensorDistance = ultrasonic.distanceRead();
-  // Serial.println(roomSensorDistance);
-
-  // if (roomSensorDistance < roomDistance) {
-
-  // 	if (counter < 2) {
-  // 		counter = counter + 1;
-  // 	} else {
-
-	 //  	if (!lightIsOn) {
-		//     digitalWrite(led1, HIGH);
-		//     lightIsOn = true;
-	 //  	}
-
-	 //    lightOffTimer = LIGHT_OFF_TIMER_VALUE;
-	 //    counter = 0;
-
-  // 	}
-
-
-  // } else {
-
-  // 	if (lightIsOn) {
-
-	 //  	if (lightOffTimer > 0) {
-	 //  		lightOffTimer = lightOffTimer - MAIN_CYCLE_DELAY;
-	 //  	} else {
-	 //    	digitalWrite(led1, LOW);
-	 //    	lightIsOn = false;
-	 //  	}
-  // 	}
-  // 	counter = 0;
-
-  // }
-  
 	delay(MAIN_CYCLE_DELAY);
 }
